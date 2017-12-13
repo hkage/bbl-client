981ee06a130cc300cd7e580e7bc13a6d8ce77c2d
Henning Kage <henning.kage@gmail.com>
2017-12-13 18:23:10 +0100

    Cleanup

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 var chalk = require('chalk');
 var co = require('co');
-var prompt = require('co-prompt');
 var program = require('commander');
-var fs = require('fs');
-//var request = require('superagent');
-var request = require('request');
+var prompt = require('co-prompt');
+var request = require('superagent');
+
+const urlShowBoulder = 'http://climbercontest.de/bbl2017/showBoulder.php';
 
 program
     .version('0.0.1')
@@ -16,23 +16,11 @@ program
 program
     .command('show [color] [boulder]', 'Display')
     .action(function(color, boulder) {
-        console.log(chalk.green('Displaying perpetration for ' + boulder));
-        console.log(chalk.bold.green('Foo'));
-
-        request('http://www.google.com/asdad', function (error, response, body) {
-            console.log('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
-          });
-
-        //request
-            //.get('http://climbercontest.de/bbl2017/showBoulder.php')
-            //.send({farb_key: color, boulder: boulder})
-            //.set('Accept', 'text/html')
-            //.end((err, res) => {
-            //    console.log(chalk.green(res));
-            //});
-        
+        console.log('Displaying perpetration for boulder', boulder);
+        request
+            .post(urlShowBoulder)
+            .send({farb_key: color, boulder: boulder})
+            .set('Content-Type', 'text/html');
         process.exit(0);
     });
 
