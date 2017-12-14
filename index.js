d3ce47e4ce71e07663dd9aeeee80d6d6f06d74ae
Henning Kage <henning.kage@gmail.com>
2017-12-14 09:56:24 +0100

    Display the list of ascents and use human readable color codes

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 #!/usr/bin/env node
 
 var chalk = require('chalk');
+var cheerio = require('cheerio');
 var co = require('co');
 var program = require('commander');
 var prompt = require('co-prompt');
 var request = require('request');
 
 const urlShowBoulder = 'http://climbercontest.de/bbl2017/showBoulder.php';
+
+const mapping = {
+    'orange': [1, 1],
+    'gelb': [2, 1],
+    'grün': [3, 2],
+    'rot': [4, 3],
+    'blau': [5, 4],
+    'grau': [6, 5],
+    'schwarz': [7, 6],
+    'pink': [8, 3]
+};
+
+map = function(color, callback) {
+    var entry = mapping[color];
+    if (entry) {
+        return callback(entry[0], entry[1]);
+    }
+    else {
+        console.log(chalk.red('Wrong color, possible choices: ' + Object.keys(mapping).join(',')));
+    }
+};
 
 
 program
@@ -18,12 +40,17 @@ program
     .command('show <color> <boulder>')
     .description('Display all ascents of a boulder')
     .action(function (color, boulder) {
-        console.log('Displaying perpetration for boulder', boulder);
-        request
-            .post({url: urlShowBoulder, form: {farb_key: color, boulder: boulder}}, 
-                function(err, httpResponse, body){
-                    console.log(body)
-                });
+        map(color, function(color_code, points) {
+            request
+                .post({url: urlShowBoulder, form: {farb_key: color_code, boulder: boulder}}, 
+                    function(err, httpResponse, body){
+                        var $ = cheerio.load(body)
+                        $('.nameBoulder').each(function(i, elem) {
+                            console.log($(this).text());
+                        });
+                    });
+        });
+        
         //process.exit(0);
     });
 
